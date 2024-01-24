2c0aac7981f32122dff387b43a71c8bf3d6870e9
DamaniN <37876601+DamaniN@users.noreply.github.com>
2024-01-23 21:41:03 -0800

    Updated required versions of modules.

--- provider.tf
+++ provider.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~>5.0"
+      version = "~>5.26.0"
     }
     polaris = {
       source  = "rubrikinc/polaris"
-      version = "=0.8.0-beta.7"
+      version = "=0.8.0-beta.8"
     }
   }
 }
