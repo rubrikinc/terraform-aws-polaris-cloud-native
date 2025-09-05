cdade21acb0febf959a01e967951628a556a14c6
Johan Andersson <johan.andersson@rubrik.com>
2025-09-05 16:28:55 +0200

    Clean up module
    * Relax the AWS provider version constraint to >=5.26.0.
    * Relax the RSC (Polaris) provider version constraint to >=1.0.0.
    * Remove the AWS and RSC (Polaris) provider blocks from the
      module. These must now be provided in the Terraform root module.
    * Add module usage examples.
    * Mark the aws_profile and rsc_credentials variables as
      deprecated. They are no longer used by the module and have no
      replacements.
    * Add aws_iam_role_type input variable to support different ways to
      attach policies to the IAM roles created. Possible values are
      legacy, inline and managed. Defaults to managed. Legacy should only
      be used for backwards compatibility with previously onboarded AWS
      accounts.
    * Add support for specifying additional tags to the resources being
      created in AWS.

---
+++ examples/basic/README.md
@@ -0,0 +1,50 @@
+# Basic AWS account onboarding
+The configuration in this directory adds an AWS account to RSC.
+
+## Usage
+To run this example you need to execute:
+```bash
+$ terraform init
+$ terraform plan
+$ terraform apply
+```
+Note that this example may create resources which can cost money. Run `terraform destroy` when you don't need these
+resources.
+
+<!-- BEGIN_TF_DOCS -->
+
+
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | >=5.26.0 |
+| <a name="requirement_polaris"></a> [polaris](#requirement\_polaris) | >=1.0.0 |
+
+## Providers
+
+No providers.
+
+## Resources
+
+No resources.
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_cloud_native"></a> [cloud\_native](#module\_cloud\_native) | ../.. | n/a |
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_aws_account_id"></a> [aws\_account\_id](#input\_aws\_account\_id) | AWS account ID. | `string` | n/a | yes |
+| <a name="input_aws_account_name"></a> [aws\_account\_name](#input\_aws\_account\_name) | AWS account name. | `string` | n/a | yes |
+| <a name="input_aws_regions"></a> [aws\_regions](#input\_aws\_regions) | AWS regions. | `list(string)` | n/a | yes |
+
+## Outputs
+
+No outputs.
+
+<!-- END_TF_DOCS -->
---
+++ examples/basic/main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.26.0"
+    }
+    polaris = {
+      source  = "rubrikinc/polaris"
+      version = ">=1.0.0"
+    }
+  }
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "AWS account ID."
+}
+
+variable "aws_account_name" {
+  type        = string
+  description = "AWS account name."
+}
+
+variable "aws_regions" {
+  type        = list(string)
+  description = "AWS regions."
+}
+
+provider "aws" {}
+
+provider "polaris" {}
+
+module "cloud_native" {
+  source = "../.."
+
+  aws_account_id   = var.aws_account_id
+  aws_account_name = var.aws_account_name
+  aws_regions      = var.aws_regions
+
+  rsc_aws_features = [
+    {
+      name              = "CLOUD_NATIVE_ARCHIVAL",
+      permission_groups = ["BASIC"]
+    },
+    {
+      name              = "CLOUD_NATIVE_PROTECTION"
+      permission_groups = ["BASIC"]
+    },
+    {
+      name              = "CLOUD_NATIVE_S3_PROTECTION"
+      permission_groups = ["BASIC"]
+    },
+    {
+      name              = "EXOCOMPUTE"
+      permission_groups = ["BASIC", "RSC_MANAGED_CLUSTER"]
+    },
+    {
+      name              = "RDS_PROTECTION",
+      permission_groups = ["BASIC"]
+    },
+  ]
+
+  tags = {
+    Environment = "test"
+    Example     = "basic"
+    Module      = "terraform-aws-polaris-cloud-native-exocompute-networking"
+  }
+}
